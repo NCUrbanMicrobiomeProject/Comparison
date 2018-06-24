c676e1d3e3a229b145351eab552691c811ba80a4
dmashruwala <dmashruwala@outlook.com>
2018-06-23 23:17:30 -0400

    Uploaded new code updates.

--- Dev_06022018.R
+++ Dev_06022018.R
@@ -1,14 +1,21 @@
-setwd("C:/Users/dmash/Documents/R Microbiome Code/ NC Urban Microbiome Project")
+setwd("C:/Users/dmash/Documents/R Microbiome Code/NC Urban Microbiome Project")
 library(ggplot2)
 library(reshape2)
 input_files <- list.files(pattern=".txt")
 
 for(i in 1:length(input_files)){
-   j <- read.table(input_files[i], header=TRUE)
-   k <- input_files[i].startswith("MetaPhlAn")
-   if(k = TRUE) {
-     melted <- melt(j, id.vars=colnames(j)[1], measure.vars=colnames(j)[48:length(j.cols)])
+  j <- read.table(input_files[i], header=TRUE)
+  k <- startsWith(input_files[i], "MetaPhlAn")
+  if(k == TRUE) {
+    meltmeta <- melt(j, id.vars=colnames(j)[1], measure.vars=colnames(j)[48:ncol(j)])
+    input_files.plot <- ggplot(data=meltmeta) + aes(x=Row.names, fill=variable) + geom_bar()
+    ggsave(filename = input_files, plot = last_plot(), device = "pdf", path = "C:/Users/dmash/Desktop", scale = 1, width = NA, height = NA, dpi = 300, limitsize = FALSE)
   } else{
-     melted <- melt(j, id.vars=colnames(j)[1], measure.vars=colnames(j)[50:length(j.cols)])
+    meltqiime <- melt(j, id.vars=colnames(j)[1], measure.vars=colnames(j)[51:ncol(j)])
+    input_files.plot <- ggplot(data=meltqiime) + aes(x=Row.names, fill=variable) + geom_bar()
+    ggsave(filename = input_files.pdf, plot = last_plot(), device = "pdf", path = "C:/Users/dmash/Desktop", scale = 1, width = NA, height = NA, dpi = 300, limitsize = FALSE)
+  }
 }
-}  
+
+# make sure you state when the metadata ends for qiime and metaphlan (using if else?)
+# ggsave
