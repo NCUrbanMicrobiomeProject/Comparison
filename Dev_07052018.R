8517887e2934008b97aec57381691ee73d09395b
dmashruwala <dmashruwala@outlook.com>
2018-07-09 10:51:54 -0400

    Changed to where the bars are stacked and the x-axis is Sample Location. Changed height and width on Qiime plot so I could actually see the graph. Not 100% finished, still working on the y-axis.

--- Dev_07052018.R
+++ Dev_07052018.R
@@ -7,35 +7,33 @@ for(i in 1:length(input_files)){
   k <- startsWith(input_files[i], "MetaPhlAn")
   if(k == TRUE) {
     start <- 48
-    meltmeta <- melt(j, id.vars=colnames(j)[1], measure.vars=colnames(j)[start:ncol(j)])
+    meltmeta <- melt(j, id.vars=colnames(j)[4], measure.vars=colnames(j)[start:ncol(j)])
     attach(meltmeta)
     sortmeltmeta <- meltmeta[order(-value),]
     require(data.table)
     metagroup <- data.table(sortmeltmeta)
     metamax <- metagroup[metagroup[, .I[value == max(value)], by=variable]$V1]
-    topmeta <- data.frame(metamax[1:10])
-    input_files.plot <- ggplot(data=topmeta) + aes(x=rownames(topmeta, do.NULL = TRUE, prefix = "Row"), fill=variable) + geom_bar()
-    ggsave(filename = paste("metaphlan.pdf"), plot = input_files.plot, device = "pdf", path = "~", scale = 0.25, width = NA, height = NA, dpi = 300, limitsize = TRUE)
+    topmeta <- data.frame(metamax[1:30])
+    input_files.plot <- ggplot(data=topmeta) + aes(x=Sample.Location, fill=variable) + geom_bar()
+    ggsave(filename = paste("metaphlan.pdf"), plot = input_files.plot, device = "pdf", path = "~", width = NA, height = NA, dpi = 300, limitsize = TRUE)
 
     
     
     
   } else{
     start <- 51
-    meltqiime <- melt(j, id.vars=colnames(j)[1], measure.vars=colnames(j)[start:ncol(j)])
+    meltqiime <- melt(j, id.vars=colnames(j)[4], measure.vars=colnames(j)[start:ncol(j)])
     attach(meltqiime)
     sortmeltqiime <- meltqiime[order(-value),]
     require(data.table)
     qiimegroup <- data.table(sortmeltqiime)
     qiimemax <- qiimegroup[qiimegroup[, .I[value == max(value)], by=variable]$V1]
-    topqiime <- data.frame(qiimemax[1:10])
-    input_files.plot <- ggplot(data=topqiime) + aes(x=rownames(topqiime, do.NULL = TRUE, prefix = "Row"), fill=variable) + geom_bar()
-    ggsave(filename = paste("qiime.pdf"), plot = input_files.plot, device = "pdf", path = "~", scale = 0.25, width = NA, height = NA, dpi = 300, limitsize = TRUE)
+    topqiime <- data.frame(qiimemax[1:20])
+    input_files.plot <- ggplot(data=topqiime) + aes(x=Sample.Location, fill=variable) + geom_bar()
+    ggsave(filename = paste("qiime.pdf"), plot = input_files.plot, device = "pdf", path = "~", width = 10, height = 10, dpi = 300, limitsize = TRUE)
     
   }
 }
 
-# make sure you state when the metadata ends for qiime and metaphlan (using if else?)
-# ggsave
 
 
