00bba2d2de5703e736a86567878ae413fb5ba3a0
dmashruwala <dmashruwala@outlook.com>
2018-07-27 09:39:28 -0400

    Changed how R searches the filenames. (see line 8).

--- Dev_07102018.R
+++ Dev_07102018.R
@@ -5,7 +5,7 @@ input_files <- list.files(pattern=".txt")
 
 for(i in 1:length(input_files)){
   j <- read.table(input_files[i], header=TRUE)
-  k <- startsWith(input_files[i], "MetaPhlAn")
+  k <- grepl("metaphlan", input_files[i], ignore.case = TRUE)
   if(k == TRUE) {
     start <- 48
     meltmeta <- melt(j, id.vars=colnames(j)[4], measure.vars=colnames(j)[start:ncol(j)])
@@ -17,8 +17,8 @@ for(i in 1:length(input_files)){
     require(data.table)
     metagroup <- data.table(sortmeltmeta)
     metamax <- metagroup[metagroup[, .I[value == max(value)], by=variable]$V1]
-    topmeta <- data.frame(metamax[1:15])
-    input_files.plot <- ggplot(data=topmeta) + aes(y=value, x=Sample.Location, fill=variable) + geom_bar(stat = "identity")
+    topmeta <- data.frame(metamax[1:30])
+    input_files.plot <- ggplot(data=topmeta) + aes(y=value, x=Sample.Location, fill=variable) + geom_bar(stat = "identity") + theme(legend.position = "")
     ggsave(filename = paste("plot", input_files[i], ".pdf"), plot = input_files.plot, device = "pdf", path = "C:/Users/dmash/OneDrive/Documents/R Microbiome Code/NC Urban Microbiome Project/metaphlan_plots", width = NA, height = NA, dpi = 300, limitsize = TRUE)
     
     
@@ -33,8 +33,8 @@ for(i in 1:length(input_files)){
     require(data.table)
     qiimegroup <- data.table(sortmeltqiime)
     qiimemax <- qiimegroup[qiimegroup[, .I[value == max(value)], by=variable]$V1]
-    topqiime <- data.frame(qiimemax[1:15])
-    input_files.plot <- ggplot(data=topqiime) + aes(y=value, x=Sample.Location, fill=variable) + geom_bar(stat = "identity")
+    topqiime <- data.frame(qiimemax[1:30])
+    input_files.plot <- ggplot(data=topqiime) + aes(y=value, x=Sample.Location, fill=variable) + geom_bar(stat = "identity") + theme(legend.position = "")
     ggsave(filename = paste("plot", input_files[i], ".pdf"), plot = input_files.plot, device = "pdf", path = "C:/Users/dmash/OneDrive/Documents/R Microbiome Code/NC Urban Microbiome Project/qiime_plots", width = 10, height = 10, dpi = 300, limitsize = TRUE)
     
   }
