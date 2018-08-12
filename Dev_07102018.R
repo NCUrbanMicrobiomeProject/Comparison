0f5e133ba28f0d638d9ab9f848cda5c026250d7d
dmashruwala <dmashruwala@outlook.com>
2018-08-12 19:18:23 -0400

    Changed so it gives top 20 relative abundance values, and scaled the MetaPhlAn RA values.

--- Dev_07102018.R
+++ Dev_07102018.R
@@ -13,11 +13,12 @@ for(i in 1:length(input_files)){
     write.table(meltmeta, file = paste0("melt", input_files[i]), sep = "\t", row.names = FALSE, append = FALSE)
     setwd("C:/Users/dmash/OneDrive/Documents/R Microbiome Code/NC Urban Microbiome Project/")
     attach(meltmeta)
+    meltmeta$value <- (meltmeta$value/100)
     sortmeltmeta <- meltmeta[order(-meltmeta$value),]
     require(data.table)
     metagroup <- data.table(sortmeltmeta)
     metamax <- metagroup[metagroup[, .I[value == max(value)], by=variable]$V1]
-    topmeta <- data.frame(metamax[1:30])
+    topmeta <- data.frame(metamax[1:20])
     input_files.plot <- ggplot(data=topmeta) + aes(y=value, x=Sample.Location, fill=variable) + geom_bar(stat = "identity") + theme(legend.position = "")
     ggsave(filename = paste("plot", input_files[i], ".pdf"), plot = input_files.plot, device = "pdf", path = "C:/Users/dmash/OneDrive/Documents/R Microbiome Code/NC Urban Microbiome Project/metaphlan_plots", width = NA, height = NA, dpi = 300, limitsize = TRUE)
     
@@ -33,7 +34,7 @@ for(i in 1:length(input_files)){
     require(data.table)
     qiimegroup <- data.table(sortmeltqiime)
     qiimemax <- qiimegroup[qiimegroup[, .I[value == max(value)], by=variable]$V1]
-    topqiime <- data.frame(qiimemax[1:30])
+    topqiime <- data.frame(qiimemax[1:20])
     input_files.plot <- ggplot(data=topqiime) + aes(y=value, x=Sample.Location, fill=variable) + geom_bar(stat = "identity") + theme(legend.position = "")
     ggsave(filename = paste("plot", input_files[i], ".pdf"), plot = input_files.plot, device = "pdf", path = "C:/Users/dmash/OneDrive/Documents/R Microbiome Code/NC Urban Microbiome Project/qiime_plots", width = 10, height = 10, dpi = 300, limitsize = TRUE)
     
